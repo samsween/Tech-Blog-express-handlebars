41279752cb6b037f1b1bee7ae8e6122eb9e7c602
Sam Sweeney <samsweeneyy@gmail.com>
2023-03-16 11:43:15 +1100

    Cahnged webpack output path

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'public/dist'),
     filename: 'bundle.js',
   },
   devServer: {
