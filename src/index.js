1097faaf22c774de3fbc17dc491fd096e884dacf
Sam Sweeney <samsweeneyy@gmail.com>
2023-03-16 14:09:04 +1100

    Added favicon, fixed styling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 
 import {indexJs} from './js';
 import "./styles.css"
+window.onload = () => {
+    const container = document.querySelector('html');
+    const toggle = document.querySelector('#flexSwitchCheckDefault');
+    const dark = localStorage.getItem('dark');
+    if (dark === 'true') {
+        container.classList.add('dark');
+        toggle.checked = true;
+    }
+}
+const container = document.querySelector('html');
+const toggle = document.querySelector('#flexSwitchCheckDefault');
+toggle.addEventListener('change', () => {
+    localStorage.setItem('dark', toggle.checked);
+    container.classList.toggle('dark');
+});
 indexJs();
